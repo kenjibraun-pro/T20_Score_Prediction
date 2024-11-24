1f04e82b339617d9f2017ec0199e2223c0557659
kenjibraun-pro <kenjibraun.pro@gmail.com>
2024-11-23 20:13:26 -0500

    updated

--- app.py
+++ app.py
@@ -104,14 +104,4 @@ with col5:
 
 last_five = st.number_input('Runs Scored in last 5 Overs',min_value=0,max_value=None, value=0, step=1)
 
-if st.button('Predict Score'):
-    balls_left = 120 - (overs * 6)
-    wickets_left = 10 - wickets
-    crr = current_score / overs
 
-    input_df = pd.DataFrame(
-        {'batting_team': [batting_team], 'bowling_team': [bowling_team], 'city': [city],
-         'current_score': [current_score], 'balls_left': [balls_left], 'wickets_left': [wickets], 'crr': [crr],
-         'last_five': [last_five]})
-    result = pipe.predict(input_df)
-    st.header("Predicted Score : " + str(int(result[0])))
